d32932877e4d631fb035ef54aec3d2cbaf2507ab
Andrew <andrewattfield2605@gmail.com>
2026-01-15 21:44:57 -0500

    Update Account.java

--- Account.java
+++ Account.java
@@ -2,47 +2,17 @@ package Lab8;
 import java.util.Date;
 import java.util.Objects;
 
-
-/* when you implement Comparable, it is possible that a warning
- * is given, which you should ignore for now, until we discuss Generics. 
- */
 public abstract class Account implements Comparable {
 	protected int accountNo;
-	// add the rest of attributes here
 
 	protected double balance;
 	protected String fullName;
 	protected Date dateOpened;
 	protected double maxTransferable;
 	
-	/**
-	 * This method deposit <code> amount </code> to this account.
-	 * @param amount is the amount that is deposited to this account. 
-	 */
-	// insert your code here
-	
 	public abstract void deposit(double amount); 
 		
-	
-	/**
-	 * This method withdraw <code> amount </code> from this account.
-	 * @param amount is the amount that should be withdrawn from this account
-	 * @return It returns true if the transaction is done successfully. 
-	 * @throws Exception This methods may throw either <code> NotEnoughMoneyException </code> or
-	 * <code> TransferNotAllowedException </code>.
-	 */
 	public abstract boolean withdraw(double amount) throws Exception;
-
-	/**
-	 * This method transfers money from this account to the given account.
-	 * @param to is the destination account, where the money is deposited to.
-	 * @param amount is the amount of money that is transfered. 
-	 * @return It returns true if the transaction is successful. 
-	 * @throws Exception This methods may throw either <code> NotEnoughMoneyException </code> or
-	 * <code> TransferNotAllowedException </code>.
-	 */
-
-	// insert your code here
 	
 	public boolean transferFrom(Account to, double amount) throws Exception {
 		
@@ -53,17 +23,6 @@ public abstract class Account implements Comparable {
 		return true;
 		
 	}
-	
-	
-	
-	
-	
-	/**
-	 * This is the accessor method for <code> accountNo </code>
-	 * @return It returns the accountNo of this account.
-	 */
-
-	// insert your code here
 
 	public int getAccountNo() {
 		
@@ -71,84 +30,22 @@ public abstract class Account implements Comparable {
 		
 	}
 	
-	
-	
-	
-	/**
-	 * This is the accessor method for <code> balance </code>
-	 * @return It returns the balance of the account.
-	 */
-
-	
-	// insert your code here
-	
 	public double getBalance() {
 		return this.balance;
 	}
-
-	
-	
-	
-	/**
-	 * This is the accessor method for <code> fullName </code>
-	 * @return It returns the name of the holder of the account.
-	 */
-
-	// insert your code here
 	
 	public String getFullName() {
 		return this.fullName;
 	}
-
-	
-	
-	
-	
-	/**
-	 * This is the accessor method for <code> dateOpened </code>
-	 * @return It returns the date at which the account was opened.
-	 */
-
-	// insert your code here
 	
 	public Date getDateOpened() {
 		return this.dateOpened;
 		
 	}
-
-	
-	
-	
-	
-	
-	/**
-	 * This is the accessor method for <code> maxTransferable </code>
-	 * @return It returns the maximum allowed amount that can be withdrawn from this account in one transaction.
-	 */
-
-	// insert your code here
 	
 	public double getMaxTransferable() {
 		return this.maxTransferable;
 	}
-
-	
-	
-	
-	
-	
-	/**
-	 * This method compares two accounts. If the two accounts have the same values
-	 * for all the instance variables, they are considered, equal and this 
-	 * method returns 0. If two objects were not equal, the account whose accountNo is less, 
-	 * is the smaller object so this method returns -1. Otherwise it returns 1.
-	 * @param object is an object of type account. 
-	 * @return<pre> It returns 0, if the two objects are equal. 
-	 * It returns -1, if this object is less than the object that is passed as a parameter into the method. 
-	 * It returns 1, if this object is greater than the object that is passed as a parameter into the method<pre>. 
-	 */
-
-	// insert your code here
 	
 	@Override
 	public abstract int compareTo(Object o);
@@ -157,8 +54,6 @@ public abstract class Account implements Comparable {
 	
 	
 class Current extends Account {
-	
-	// insert your code here
 	
 	public Current(int accountNo, double balance, String fullName, Date dateOpened, double maxTransferable) {
 		this.accountNo = accountNo;
@@ -259,13 +154,6 @@ class Current extends Account {
 	
 }
 
-/**
- * This class is a user defined Exception used 
- * when transferring money is not allowed.
- *
- */
-	// insert your code here
-
 class TransferNotAllowedException extends Exception {
 	public TransferNotAllowedException() {}
 	public TransferNotAllowedException(String message) {
@@ -277,22 +165,12 @@ class TransferNotAllowedException extends Exception {
 	
 }
 
-
-
-/**
- * This is a user defined exception used
- * when a transaction is unsuccessful due to lack
- * of enough money.
- */
-	// insert your code here
-
 class NotEnoughMoneyException extends TransferNotAllowedException {
 	public NotEnoughMoneyException() {}
 	public NotEnoughMoneyException(String message) {
 		super(message);
 		
 	}
-	
-	
 }
+
 
